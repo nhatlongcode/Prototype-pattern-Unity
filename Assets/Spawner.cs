66abf97c2f26af0c20e31525d9dca4263ef7a154
Nguyen Nhat Long <nhatlongcode@gmail.com>
2020-03-16 01:37:35 +0700

    Init repo

---
+++ Assets/Spawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public GameObject spherePrefabs;
+    private void Update()
+    {
+        if (Random.Range(0,100) < 50)
+        Instantiate(cubePrefab, this.transform.position, Quaternion.identity);
+        //else 
+        //Instantiate(spherePrefabs, this.transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Spawner2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner2 : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 100) < 50) ProcCube.CloneSphere(this.transform.position);
+    }
+}
